3f7ce485015f6eadc52a9ef27a668ea87771347a
WACLOT <105559019+PogZimi@users.noreply.github.com>
2023-05-09 21:25:05 +0530

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -6,14 +6,8 @@ class Solution {
 public:
     bool isPalindrome(string number) {
          int size = number.size();
-         string nums = "0123456789";
          string palindrome_num, o;
-         
-         for(int i = 0; i < size; i++){
-             if(nums[i] != number[i]){
-                     
-             }             
-         }
+        
          for(int i = size-1; i > -1 ; i--){
                o = number[i];
                palindrome_num += o;
