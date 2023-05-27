17c247834758837c49441cf3e7dec228914bc351
WACLOT <105559019+PogZimi@users.noreply.github.com>
2023-05-27 12:55:36 +0530

    Update 3sum.py

--- 3sum.py
+++ 3sum.py
@@ -1,8 +1,23 @@
+'''
+
+-> Main loops : 2
+-> Nested Loops : 2
+
+IMPORTANT STUFF : 
+
+-> threeSumList is the list in which we store the 3 integers whose sum is equal to target value 
+-> Since the threeSumList stores duplicates , we created a new list 'deduplicated_threeSumList'.
+-> threeSumList stores sub lists. In simple words , we created a multi-dimensional array/list 
+-> We sorted each element ( sub-list) 
+-> We used set() to remove duplicates & obtain unique values 
+
+'''
+
 class Solution(object):
     def threeSum(self, nums):
         target = 0
         var = 0
-        Q = []
+        threeSumList = []
         self.size = nums.__len__()
         nums.sort()
         
@@ -15,9 +30,9 @@ class Solution(object):
                 if((target-var) in nums):
                   for x in range(self.size):
                     if(nums[x] == target-var and x!=i and x!=j ):
-                        Q.append([nums[i], nums[x], nums[j]])
+                        threeSumList.append([nums[i], nums[x], nums[j]])
 
-        for i in range(Q.__len__()):
-            Q[i].sort()      
-        O = set(tuple(lst) for lst in Q)         
-        return O
+        for i in range(threeSumList.__len__()):
+            threeSumList[i].sort()      
+        deduplicated_threeSumList = set(tuple(lst) for lst in threeSumList)         
+        return deduplicated_threeSumList
