5258f5f320aa87a285a9b3d4a6194b7f18520dbe
Dr.XenTissue <105559019+PogZimi@users.noreply.github.com>
2024-03-31 21:33:10 +0530

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -4,21 +4,29 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPalindrome(string number) {
-         int size = number.size();
-         string palindrome_num, o;
-        
-         for(int i = size-1; i > -1 ; i--){
-               o = number[i];
-               palindrome_num += o;
-           } 
+    bool isPalindrome(int x) {
+         string n = to_string(x);
+         bool isPalindromer=false;
+         int n_size = n.size();
+         int q_ = n_size;
+         vector<string>num;
+         for(int i =0;i < n_size; i++){
+              num.push_back(string(1, n[i]));
+         }
 
-         if(palindrome_num == number){
-                return true;
-             }
-         else{
-                return false;
-            }
+
+         for(int i = 0; i < n_size; i++ ){
+              if(num[i] != num[q_ - 1]){
+                     isPalindromer=false;
+                     q_--;
+                     break;
+              }   
+              else{
+                     isPalindromer=true;
+                     q_--;
+              }
+         }
+         return isPalindromer;
     }
 };
 
@@ -27,8 +35,8 @@ int main(){
     Solution o;
     int x; 
     cin >> x; 
-    bool flock = o.isPalindrome(to_string(x));
-
+    bool flock = o.isPalindrome(x);
+      
     if(flock == 1){
          cout << "true\n"; 
     }
